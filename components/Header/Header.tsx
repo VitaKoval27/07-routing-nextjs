624a2ad3182908a65d9acebdaebd8d9076839b34
VitaKoval27 <talisman1980@gmail.com>
2025-12-07 16:29:57 +0000

    Fix link path for Notes in Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
           <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="./notes/filter/all">Notes</Link>
+          <Link href="/notes/filter/all">Notes</Link>
         </li>
       </ul>
     </nav>
